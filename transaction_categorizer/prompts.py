bc797f6137404e1aee0b3b0115a8d9f702511b18
tony.liu <tony.liu@klarna.com>
2024-02-18 23:12:20 +0100

    Issue #3 Adding example category definition and using pre-defined categories by passing definition to prompt

--- transaction_categorizer/prompts.py
+++ transaction_categorizer/prompts.py
@@ -1,14 +1,19 @@
+from config import format_categories_for_prompt, parse_categories
 from langchain_core.prompts import PromptTemplate
 
 
 def get_prompt(structured_parser):
+    formatted_categories = format_categories_for_prompt(parse_categories())
     return PromptTemplate(
-        template="""
+        template=f"""
         You are an personal financial assistant that categorizes 
         bank transfers for transactions in Sweden. The categories
         should be useful for managing and visualizing personal budget 
-        and expenses. Categorize the following transaction: "{description}".
-        {format_instructions}
+        and expenses. Categorize the following transaction: "{{description}}".
+        {{format_instructions}}
+        categories and subcategories should be one of the categories provided 
+        in this definition: 
+        {formatted_categories}
         """,
         input_variables=["description"],
         partial_variables={
